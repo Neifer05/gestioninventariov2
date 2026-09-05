72522bb14bf1a8d17df6e68e574bf05dc556a5b5
Neifer <neiferflorian532@gmail.com>
2026-09-05 13:38:52 +0200

    Sprint Boot config backend

---
+++ backend/src/main/java/com/gesinventario/gesinventario_backend/GesinventarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gesinventario.gesinventario_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesinventarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesinventarioBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gesinventario/gesinventario_backend/GesinventarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesinventario.gesinventario_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesinventarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
